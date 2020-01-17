63a6cf5dd9a1b867e3dc0a397b888d0e9868f816
Maja Trpeska <maja.trpeska@virtualstock.co.uk>
2020-01-17 01:47:02 +0100

    Admin site registered

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Author, Post, Topic
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+    list_filter = ['first_name', 'last_name', 'email']
+    search_fields = ['first_name', 'last_name', 'email']
+
+
+class PostAdminSite(admin.ModelAdmin):
+    list_display = ['title', 'author', 'topic']
+    list_filter = ['title', 'author', 'topic']
+    search_fields = ['title', 'content']
+
+
+class TopicAdminSite(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    search_fields = ['name']
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdminSite)
+admin.site.register(Topic, TopicAdminSite)
