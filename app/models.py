419b8bc87083f3201a1644aa66059fc71e5e75b0
Maja Trpeska <maja.trpeska@virtualstock.co.uk>
2020-01-19 18:47:29 +0100

    Added models and proper migration

--- app/models.py
+++ app/models.py
@@ -1,45 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
 
 POST_STATUS_CHOICES = [
     ('draft', 'DRAFT'),
     ('published', 'PUBLISHED'),
 ]
 
-class Author(AbstractUser):
+
+class User(AbstractUser):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=252)
     email = models.EmailField(unique=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.first_name + self.last_name
-
     class Meta:
-        verbose_name_plural = 'Authors'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.first_name + '' + self.last_name
+
+    def get_user_posts(self):
+        return Post.objects.filter(user=self)
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=128)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name_plural = 'Topics'
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='topic')
     title = models.CharField(max_length=252)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=9, choices=POST_STATUS_CHOICES, default='draft')
+
+    class Meta:
+        verbose_name_plural = 'Posts'
 
     def __str__(self):
         return self.title
